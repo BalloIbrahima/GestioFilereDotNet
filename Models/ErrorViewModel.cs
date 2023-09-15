69fd6d2e8879c4d347af37ab690599a713657a2d
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-09-15 23:31:25 +0000

    init donet project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GestionFilere.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
